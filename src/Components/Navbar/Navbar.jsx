04714e6a318017760a72e85e7e1ecd874f5443a1
HishamReda1 <hishamreda206@gmail.com>
2023-05-02 08:03:28 +0200

    first commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,8 +11,8 @@ export default function Navbar({userdata,removeUser}) {
         <div className="container-fluid">
           <Link className="navbar-brand text-light" to='/'><h3>Noxe</h3></Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+      <span className="navbar-toggler-icon"></span>
+    </button>
           <div className="collapse navbar-collapse " id="navbarSupportedContent">
 
             <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
